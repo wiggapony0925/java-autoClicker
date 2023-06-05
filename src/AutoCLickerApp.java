e8199b36367ccd4eeff103dc064ac958e78ee278
wiggapony0925 <ninjeff06@gmail.com>
2023-06-04 22:37:11 -0400

    added the componets/inatences of the class

--- src/AutoCLickerApp.java
+++ src/AutoCLickerApp.java
@@ -2,5 +2,21 @@ import javax.swing.*;
 import java.awt.* ;
 
 public class AutoCLickerApp {
-    // This file will  be responsible for creating and handerling the graphical interface
+    /* This file will  be responsible for creating and dealing with the graphical interface
+    private variables
+     */
+    //      components/instances
+    private JFrame frame;
+    private JTextField cpsField;
+    private JToggleButton actionTypeToggle;
+    private Button toggleKeybindButton;
+    private JComboBox<String> mouseButtonComboBox;
+    private JButton StartButton;
+    private JButton StopButton;
+    private JButton exitButton;
+
+
+
+
+
 }
