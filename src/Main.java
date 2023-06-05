dd1bd76288e0eed0e078f9d5164d3600cfc39731
wiggapony0925 <ninjeff06@gmail.com>
2023-06-04 23:09:48 -0400

    showed the ui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        AutoClickerApp autoClickerApp = new AutoClickerApp();
+        AutoCLickerApp autoCLickerApp = new AutoCLickerApp();
+        autoCLickerApp.ShowUI();
     }
 }
