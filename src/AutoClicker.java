8a60a9afeb683b178772631df991c0e8b3fb002f
wiggapony0925 <ninjeff06@gmail.com>
2023-06-05 22:14:56 -0400

    final first method

--- src/AutoClicker.java
+++ src/AutoClicker.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.util.function.IntToDoubleFunction;
+
 public class AutoClicker {
     //instances
     private Robot robot;
@@ -12,9 +14,24 @@ public class AutoClicker {
         }
     }
     // this is the start clicking functionality     method = function   mouseButton(left, middle, right)
-    public void startClicking(int cps, int mouseButton) {
+    public void startClicking(int cps) {
         isRunning = true;
        int delay = 1000 / cps; //calc the delay im miliseconds
+
+        while (isRunning) {
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        }
+
+        try {
+            Thread.sleep(delay);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopClicking() {
+        isRunning = false;
     }
 
 
