c5ad1f5c0433deae8c423248c157813aa13458bd
wiggapony0925 <ninjeff06@gmail.com>
2023-06-05 20:41:34 -0400

    trying error

--- src/AutoClicker.java
+++ src/AutoClicker.java
@@ -1,7 +1,17 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
 public class AutoClicker {
+    private Robot robot;
 
+    public AutoClicker() {
+        try {
+            robot = new Robot();
+        }catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void startClicking(int clicksPerSecond, int mouseButton) {
 
+    }
 }
